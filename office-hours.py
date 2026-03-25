177d8deb2d51f82e7fe90a656a64282b0611a676
Andrew Kvalheim <andrew@kvalhe.im>
2026-03-24 20:25:02 -0700

    Initialize uv script

---
+++ office-hours.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env -S uv run --script
+#
+# /// script
+# requires-python = ">=3.13"
+# dependencies = []
+# ///
+
+
+def main() -> None:
+    raise NotImplementedError
+
+
+if __name__ == "__main__":
+    main()
